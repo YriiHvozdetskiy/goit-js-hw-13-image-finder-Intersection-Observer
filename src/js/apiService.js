67562e928925ef64f6533c5d0fffdc448119576a
Yrii <hvozdetskiyyrii@gmail.com>
2021-05-20 14:03:07 +0300

    Дод URLSearchParams для кращого вигляду коду

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,4 +1,5 @@
-const API_KEY = '21492726-451e87d3f3afe144072272f85';
+// const API_KEY = '21492726-451e87d3f3afe144072272f85';
+const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&';
 
 export default class ApiService {
   constructor() {
@@ -7,7 +8,15 @@ export default class ApiService {
   }
 
   fetchImage() {
-    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=20&key=${API_KEY}`;
+    const searchParams = new URLSearchParams({
+      q: this.searchQuery,
+      page: this.page,
+      per_page: 20,
+      key: '21492726-451e87d3f3afe144072272f85',
+    });
+
+    const url = `${BASE_URL}${searchParams}`;
+    // const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=20&key=${API_KEY}`;
 
     return fetch(url)
       .then(response => response.json())
